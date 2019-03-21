97ae96514f449e1b35146e49bfde132f761237ec
Kalan <kjj6198@gmail.com>
2019-03-21 23:02:07 +0800

    add basic config and constants

---
+++ app/config.js
@@ -0,0 +1,10 @@
+export default {
+  projects: [
+    {
+      path: '/piano-roll',
+      name: 'PianoRoll + MidiConvert',
+      image: 'app/assets/images/piano-roll.png',
+      description: '轉換 midi 檔並使用 Tone.js 及 canvas 視覺化的 piano roll',
+    },
+  ],
+};
---
+++ app/constants/colors.js
@@ -0,0 +1,8 @@
+import { grey } from '@material-ui/core/colors';
+
+const colors = {
+  BG_BLACK: grey['900'],
+  BORDER_GERY: grey['300'],
+};
+
+export default colors;
